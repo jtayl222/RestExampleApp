0c0524e68f3710ca7fab15dd06e5c1697fe72010
Jeff Taylor <jeftaylo@redhat.com>
2022-08-20 15:43:59 -0400

    simple log message for rest server

--- rest-client/src/main/java/com/mastertheboss/SimpleRESTEndpoint.java
+++ rest-client/src/main/java/com/mastertheboss/SimpleRESTEndpoint.java
@@ -6,24 +6,29 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/proxy")
 @ApplicationScoped
 public class SimpleRESTEndpoint {
+    Logger log = LoggerFactory.getLogger(SimpleRESTEndpoint.class);
+
     @Inject
     @RestClient
-    SimpleRESTServiceItf service;
+    SimpleRESTServiceItf simpleRESTServiceItf;
     @GET
     @Path("/text")
     public String getHello() {
-        return service.getHello();
+        log.info("getHello()");
+        return simpleRESTServiceItf.getHello();
     }
     @GET
     @Path("/json")
     @Produces(MediaType.APPLICATION_JSON)
     public SimpleProperty getPropertyJSON(){
-        return service.getPropertyJSON();
+        log.info("getPropertyJSON()");
+        return simpleRESTServiceItf.getPropertyJSON();
     } 
 }
